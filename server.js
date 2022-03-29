27f26d59e3f4d08fe39bcf56268e736a251c43c4
Seiffoudine Mogne <89651345+Seiffmogne@users.noreply.github.com>
2022-03-29 15:10:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app=express();
 app.use(express.static(__dirname+ '/dist'));
 
 app.all('*',(re,res)=>{
-res.status(200).sendFile(__dirname+ '/dist/index.html');
+res.status(200).sendFile(__dirname+ '/dist/auto/index.html');
 });
 
-const port =process.env.PORT || 8080;
+//LAst change
 
-app.listen(port);
+const port=process.env.PORT || 8080;
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log(`Listening on Port ${port} ...`);
+});
